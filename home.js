aed0ed3236fb2eeb02b30a85f99c2b4e212fdbd1
nanspro <rahulbishnoi3636@gmail.com>
2019-10-02 05:22:32 +0530

    Adding pharmacy and changing flow to account

--- home.js
+++ home.js
@@ -65,11 +65,61 @@ const purchaseFarmer = async (
   }
 };
 
+const transferPharmacy = async (
+  distributor,
+  pharmacy,
+  transferMultisig,
+  transferParams,
+  transfer
+) => {
+  let params = await algodclient.getTransactionParams();
+  let endRound = params.lastRound + parseInt(1000);
+
+  let txn = {
+    from: transferMultisig,
+    to: distributor.addr,
+    fee: params.fee,
+    amount: 600000,
+    firstRound: params.lastRound,
+    lastRound: endRound,
+    genesisID: params.genesisID,
+    genesisHash: params.genesishashb64,
+    note: algosdk.encodeObj(transfer)
+  };
+
+  //Sign wiith farmer
+  let rawSignedTxn = algosdk.signMultisigTransaction(
+    txn,
+    transferParams,
+    distributor.sk
+  ).blob;
+  //sign with second account
+  let twosigs = algosdk.appendSignMultisigTransaction(
+    rawSignedTxn,
+    transferParams,
+    pharmacy.sk
+  ).blob;
+
+  //submit the transaction
+  try {
+    let tx = await p_algodclient.sendRawTransaction(twosigs);
+    console.log("Transaction : " + tx.txId);
+    return tx.txId;
+  } catch (err) {
+    console.log("err", err); // throws undefined when send with account with no money
+  }
+};
+
 var manufacturer = getAccount(process.env.MANUFACTURER);
 console.log("Manufacturer addr: " + manufacturer.addr);
 
 var farmer = getAccount(process.env.FARMER);
 console.log("Farmer addr: " + farmer.addr);
+
+var mnemonic5 =
+  "crane discover juice screen team jar orphan increase timber hurt inherit swarm ability riot dilemma spot throw hood image broken chronic spike wing above stove";
+var medicineBatch = algosdk.mnemonicToSecretKey(mnemonic5);
+console.log("Medicine Batch addr: " + medicineBatch.addr);
 
 const farmerManufactureParams = {
   version: 1,
@@ -96,7 +146,7 @@ let purchase = {
   date: "15435434",
   ingredients: [
     {
-      name: "carrots",
+      name: "sulphonic acid",
       quantity: "10",
       price: 10000
     },
@@ -116,6 +166,7 @@ const deliverDistributor = async (
   deliveryParams,
   delivery
 ) => {
+  console.log("pol");
   let params = await algodclient.getTransactionParams();
   let endRound = params.lastRound + parseInt(1000);
   let txn = {
@@ -158,13 +209,13 @@ const deliverDistributor = async (
   }
 };
 
-const decodeNote = async (multiSig, txId) => {
+const decodeNote = async (sender, txId) => {
   console.log("Let's Decode Note");
   try {
-    let tx = await algodclient.transactionInformation(multiSig, txId);
-    console.log(tx.txId);
+    let tx = await algodclient.transactionInformation(sender, txId);
+    console.log(tx.tx);
     let encodednote = JSON.stringify(algosdk.decodeObj(tx.note), undefined, 4);
-    console.log("Decoded: " + encodednote);
+    // console.log("Decoded: " + encodednote);
     return encodednote;
   } catch (err) {
     console.log("err", err);
@@ -174,7 +225,7 @@ const decodeNote = async (multiSig, txId) => {
 var farmerManufactureMultsig = algosdk.multisigAddress(farmerManufactureParams);
 console.log("Purchase Multisig Address: " + farmerManufactureMultsig);
 
-var purchaseId = '34UTXBRGBFJAHLMPOKXTCWX2XO2E5GQGSHLDEX2LP5OOXXH2L34A';
+var purchaseId = "";
 (async () => {
   let txID = await purchaseFarmer(
     manufacturer,
@@ -188,8 +239,43 @@ var purchaseId = '34UTXBRGBFJAHLMPOKXTCWX2XO2E5GQGSHLDEX2LP5OOXXH2L34A';
 
   //waiting for one block so that transaction confirms
   params = await algodclient.getTransactionParams();
+  let endRound = params.lastRound + parseInt(1000);
+
   await algodclient.statusAfterBlock(params.lastRound + 1);
   let note = await decodeNote(farmerManufactureMultsig, txID);
+  console.log("Decoded: " + note);
+  
+  let purchaseTemp = {
+    proof: txID,
+    name: "Purchase"
+  };
+
+  let txn1 = {
+    from: manufacturer.addr,
+    to: medicineBatch.addr,
+    fee: params.fee,
+    amount: 100000,
+    firstRound: params.lastRound,
+    lastRound: endRound,
+    genesisID: params.genesisID,
+    genesisHash: params.genesishashb64,
+    note: algosdk.encodeObj(purchaseTemp)
+  };
+  console.log(purchaseTemp);
+  console.log("li");
+  let signedTxn = algosdk.signTransaction(txn1, manufacturer.sk);
+  var attestedId;
+  console.log("lissd");
+
+  // try {
+    let tx = await p_algodclient.sendRawTransaction(signedTxn.blob);
+    console.log("Sending purchase template to batch : " + tx.txId);
+    params = await algodclient.getTransactionParams();
+    await algodclient.statusAfterBlock(params.lastRound + 1);
+    note = await decodeNote(manufacturer.addr, tx.txId);
+  // } catch (err) {
+    // console.log("err", err); // throws undefined when send with account with no money
+  // }
 })().catch(e => {
   console.log(e.error);
 });
@@ -199,11 +285,6 @@ console.log("Transporter addr: " + transport.addr);
 
 var distributor = getAccount(process.env.DISTRIBUTOR);
 console.log("Distributor addr: " + distributor.addr);
-
-var mnemonic5 =
-  "kit wrestle unit coach blouse primary peanut check raise cushion primary pluck gossip absurd mountain hundred moral interest wreck until dentist rifle correct abandon stool";
-var medicineBatch = algosdk.mnemonicToSecretKey(mnemonic5);
-console.log("Medicine Batch addr: " + medicineBatch.addr);
 
 const deliveryParams = {
   version: 1,
@@ -225,7 +306,7 @@ var delivery = {
   distributor: {
     name: "Zenus"
   },
-  ingredientSource: purchaseId,
+  ingredientSource: 'EGSNDIHJ7U7IHLLVDW64EMG7Y75VECEVVRRWDUQTFVNN7RA6JRNQ',
   medicines: [
     {
       name: "Calpol",
@@ -236,9 +317,9 @@ var delivery = {
   ]
 };
 
-
+var deliverId = "";
 (async () => {
-  console.log(delivery);
+  console.log("polllklkll");
   let txID = await deliverDistributor(
     manufacturer,
     transport,
@@ -249,7 +330,10 @@ var delivery = {
   );
   console.log("In distributor");
   console.log(txID);
-  params = await algodclient.getTransactionParams();
+
+  deliverId = txID;
+  let params = await algodclient.getTransactionParams();
+  let endRound = params.lastRound + parseInt(1000);
   await algodclient.statusAfterBlock(params.lastRound + 1);
   let note = await decodeNote(deliveryMultisig, txID);
   let deliveryw = JSON.parse(note);
@@ -257,13 +341,100 @@ var delivery = {
   console.log(deliveryw);
   console.log(batchID);
 
-  let endRound = params.lastRound + parseInt(1000);
-  let attestation = {
-    deliverye: txID
+  let deliveryTemp = {
+    proof: txID,
+    name: "Delivery"
   };
+
   let txn = {
     from: distributor.addr,
-    to: batchID,
+    to: medicineBatch.addr,
+    fee: params.fee,
+    amount: 100000,
+    firstRound: params.lastRound,
+    lastRound: endRound,
+    genesisID: params.genesisID,
+    genesisHash: params.genesishashb64,
+    note: algosdk.encodeObj(deliveryTemp)
+  };
+  let signedTxn = algosdk.signTransaction(txn, distributor.sk);
+  try {
+    let tx = await p_algodclient.sendRawTransaction(signedTxn.blob);
+    console.log("Sending delivery template to batch : " + tx.txId);
+    let params = await algodclient.getTransactionParams();
+    await algodclient.statusAfterBlock(params.lastRound + 1);
+    let note = await decodeNote(distributor.addr, tx.txId);
+  } catch (err) {
+    console.log("err", err); // throws undefined when send with account with no money
+  }
+})().catch(e => {
+  console.log(e.error);
+});
+
+var pharmacy = getAccount(process.env.PHARMACY);
+console.log("Pharmacy addr: " + pharmacy.addr);
+
+const transferParams = {
+  version: 1,
+  threshold: 2,
+  addrs: [pharmacy.addr, distributor.addr]
+};
+
+var transferMultisig = algosdk.multisigAddress(transferParams);
+console.log("Transfer MultiSig: " + transferMultisig);
+
+var transferId = "";
+
+var transfer = {
+  distributor: {
+    name: "Zenus",
+    location: {
+      lat: "10",
+      lon: "90"
+    }
+  },
+  medicines: [
+    {
+      name: "Calpol",
+      quantity: 500,
+      price: 100,
+      id: medicineBatch.addr
+    }
+  ],
+  pharmcay: {
+    name: "Himalaya",
+    location: {
+      lat: "34",
+      lon: "67"
+    }
+  },
+  source: deliverId
+};
+
+(async () => {
+  let txID = await transferPharmacy(
+    distributor,
+    pharmacy,
+    transferMultisig,
+    transferParams,
+    transfer
+  );
+  console.log("In transfer");
+  console.log(txID);
+
+  transferId = txID;
+  params = await algodclient.getTransactionParams();
+  await algodclient.statusAfterBlock(params.lastRound + 1);
+  let note = await decodeNote(transferMultisig, txID);
+
+  let endRound = params.lastRound + parseInt(1000);
+  let attestation = {
+    proof: txID,
+    name: "Transfer"
+  };
+  let txn = {
+    from: pharmacy.addr,
+    to: medicineBatch.addr,
     fee: params.fee,
     amount: 100000,
     firstRound: params.lastRound,
@@ -272,19 +443,16 @@ var delivery = {
     genesisHash: params.genesishashb64,
     note: algosdk.encodeObj(attestation)
   };
-  let signedTxn = algosdk.signTransaction(txn, distributor.sk);
-  var attestedId;
+  let signedTxn = algosdk.signTransaction(txn, pharmacy.sk);
   try {
     let tx = await p_algodclient.sendRawTransaction(signedTxn.blob);
     console.log("Attestation to batch : " + tx.txId);
-    attestedId = tx.txId;
+    let params = await algodclient.getTransactionParams();
+    await algodclient.statusAfterBlock(params.lastRound + 1);
+    let note = await decodeNote(pharmacy.addr, tx.txId);
   } catch (err) {
     console.log("err", err); // throws undefined when send with account with no money
   }
-  params = await algodclient.getTransactionParams();
-  await algodclient.statusAfterBlock(params.lastRound + 1);
-  let attestedNote = await decodeNote(batchID, attestedId);
-  console.log("Decoding Attestation : " + attestedNote);
 })().catch(e => {
   console.log(e.error);
 });
